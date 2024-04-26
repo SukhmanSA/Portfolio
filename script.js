8705d2d4741530fd3a69436c9ed523df38018026
sukhman-singh <sukhman-singh@penguin>
2024-04-26 16:15:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const navbar = document.querySelector("nav");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const percent = document.querySelectorAll(".percentage");
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
+
+
+window.addEventListener("scroll",()=>{
+    if (window.pageYOffset >= navbarOffsetTop) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    
+      sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+          navbarLinks.forEach((navbarLink) => {
+            navbarLink.classList.remove("change");
+          });
+          navbarLinks[i].classList.add("change");
+        }
+      });
+})
+
+const targets = document.querySelectorAll(".bars");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            for (let i = 0; i < 7; i++) {
+                percent[i].style.width = `${progressBarPercents[i]}%`;
+            }
+        } else {
+            percent.forEach(p => p.style.width = 0);
+        }
+    });
+}, { threshold: 0.6 });
+
+targets.forEach(target => {
+    observer.observe(target);
+});
+
+
+
+
